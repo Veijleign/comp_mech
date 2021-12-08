8e5a7e9e12f111d2c3ad91dc625c9cb77ed34dbb
Ignis Sinister <hunter_of_the_world@mail.ru>
2021-12-08 22:59:01 +0300

    Add preprocessor screen

---
+++ app/src/main/java/com/example/sapr_2/Classes/Rod.kt
@@ -0,0 +1,10 @@
+package com.example.sapr_2.Classes
+
+class Rod(
+    var L: Int = 1,
+    var b: Double = 1.0,
+    var E: Double = 1.0,
+    var Sigma: Double = 1.0
+) {
+
+}
--- app/src/main/java/com/example/sapr_2/MainActivity.kt
+++ app/src/main/java/com/example/sapr_2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.sapr_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -9,6 +10,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("SaprLog", "OnCreate")
+
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.container_fragment)
@@ -17,6 +20,37 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavigationView.setupWithNavController(navController)
 
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SaprLog", "OnStart")
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SaprLog", "OnResume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SaprLog", "OnPause")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SaprLog", "OnDestroy")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("SaprLog", "OnRestart")
 
     }
 }
